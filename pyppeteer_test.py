cc9088277a7d140414e88a4b9069af721f599138
Chuang Deng <cdengcnc@outlook.com>
2022-06-10 09:09:09 +0800

    modify the spider file

--- pyppeteer_test.py
+++ pyppeteer_test.py
@@ -2,18 +2,22 @@ import asyncio
 from pyppeteer import launch
 from pyquery import PyQuery as pq
 
-async def main():
+async def weibo_top_crawler():
     browser = await launch()
     page = await browser.newPage()
     await page.goto('https://s.weibo.com/top/summary?cate=realtimehot')
     await page.waitForSelector('.data')
     doc = pq(await page.content())
     names = [item.text() for item in doc('.td-01').items()]
+    print(names)
     for key in names:
-        if key != '':
-            print(key)
+        # if key != '':
+        try:
+            print(int(key))
+        except:
+            print('wrong')
     print('Names: ', names)
     await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_until_complete(weibo_top_crawler())
 
